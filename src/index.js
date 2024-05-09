b54c58eab78bba7ac2a1732570cdc43f8974af5e
Hemanthfuture <prabhas918231@gmail.com>
2024-05-09 13:08:15 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function AddThings(){
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const [things,setThings]=React.useState(["thing1","thing2"]);
+  function Add(){
+    setThings((prevThings)=>{
+        return [...prevThings,`thing${prevThings.length+1}`]
+    })
+  }
+  const newThings = things.map((thing)=>{
+    return <h1>{thing}</h1>
+  })
+   
+  return(
+      <div className='box'>
+        <button onClick={Add}>Add Thing</button>
+        {newThings}
+      </div>
+  );
+}
+ReactDOM.render(
+  <AddThings/>,
+  document.getElementById("root")
+  );
